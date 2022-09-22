09b263204bf69fa44d06e7e68f802280f45e8dd6
PortalFan202 <anibaldonoso1@gmail.com>
2022-09-22 18:02:29 +0300

    Started coding

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+
+const URL = '911b3806a1cbe40dacf96b52c007b3f3';
+const API_KEY = '911b3806a1cbe40dacf96b52c007b3f3';
+
+
 
 function App() {
+  const [eur, setEur] = useState(0);
+  const [gbp, setGbp] = useState(0);
+  const [rate, setRate] = useState(0);
+
+  useEffect(()=> {
+    setRate(23)
+  }
+  ,[]);
+
+  async function convert(e) {
+    e.preventDefault();
+    try {
+      const response = await fetch(URL + API_KEY)
+      if (response.ok) {
+        const json = await response.json();
+        console.log(response)
+        //setRate(json.rates.GBP);
+
+        setGbp(eur * rate);
+      } else {
+        alert('Error');
+        console.log(response);
+      }
+    } catch (e) {
+      alert(e);
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='container'>
+      <form onSubmit={convert()}>
+        <div>
+          <label htmlFor="">Euros</label>
+          <input type="number" value={eur} onChange={e => setEur(e.target.value)} />
+          <output>{rate}</output>
+        </div>
+        <div>
+          <label htmlFor="">Pounds</label>
+          <output>{gbp}</output>
+          <button onClick={convert()}>Convert</button>
+
+        </div>
+      </form>
     </div>
   );
 }
